77d37b1211efb4636a52416362eef523a3df407c
Jhony Freitas <jhony00._@hotmail.com>
2024-03-25 20:52:11 -0300

    Criando componente de título

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styles from './style';
 
 export default function Title() {
     return (
-        <View>
-            <Text>DevHealth</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>
+                DevHealth
+            </Text>
         </View>
     );
 };
