c7cfe5299935484a9fccea48cf2bf09d53d50614
Jhony Freitas <jhony00._@hotmail.com>
2024-04-02 20:54:17 -0300

    Refatorando componente

--- src/components/ResultImc/index.js
+++ src/components/ResultImc/index.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, Share } from 'react-native';
 import styles from './style';
 
-export default function ResultImc({ messageResultImc, result }) {
+export default function ResultImc({ result }) {
     const onShare = async () => {
         const res = await Share.share({
             message: `Meu imc hoje é ${result}`
@@ -10,17 +10,16 @@ export default function ResultImc({ messageResultImc, result }) {
 
     return (
         <View style={styles.imcContainer}>
-            {result &&
-                <View style={styles.shareButtonContainer}>
-                    <TouchableOpacity style={styles.sharedButton}>
-                        <Text style={styles.sharedText} onPress={onShare}>
-                            Compartilhar
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            }
-            <Text style={styles.information}>{messageResultImc}</Text>
+            <Text style={styles.information}> Seu imc é igual: </Text>
             <Text style={styles.imcNumber}>{result}</Text>
+            
+            <View style={styles.shareButtonContainer}>
+                <TouchableOpacity style={styles.sharedButton}>
+                    <Text style={styles.sharedText} onPress={onShare}>
+                        Compartilhar
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
